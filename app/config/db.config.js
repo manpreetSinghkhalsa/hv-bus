d551fa9aadc5e5ff445ae4aaf205ee32a72da2dc
Manpreet Singh <manpreet.singh@onedirect.in>
2020-08-15 12:15:17 +0530

    prod db url

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,6 @@
+// module.exports = {
+//   url: "mongodb://0.0.0.0:27017/my_db"
+// };
 module.exports = {
-  url: "mongodb://0.0.0.0:27017/my_db"
+  url: "mongodb://heroku_pv757gs3:u1tdrkbkpc8idnpfthk72q6i05@ds261430.mlab.com:61430/heroku_pv757gs3"
 };
